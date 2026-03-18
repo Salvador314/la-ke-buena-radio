e00b18cf70bfe7651ca536175a20ef3a9382192f
Salvador314 <salvador.mezalopez@fhnw.edu>
2026-03-18 10:09:14 -0500

    added changes

--- src/components/socialMedia.tsx
+++ src/components/socialMedia.tsx
@@ -10,8 +10,8 @@ import { link } from 'fs';
 
 export default function SocialSideBar(){
     const socials = [
-        {icon: <SiFacebook size={25}/>, link: 'https://facebook.com', color: '#1877F2', label: 'Facebook'},
-        {icon: <SiInstagram size={25}/>, link: 'https://instagram.com', color: '#E4405F', label: 'Instagram'},
+        {icon: <SiFacebook size={25}/>, link: 'https://www.facebook.com/la.ke.buena.kansas', color: '#1877F2', label: 'Facebook'},
+        {icon: <SiInstagram size={25}/>, link: 'https://www.instagram.com/buenakansas/', color: '#E4405F', label: 'Instagram'},
         {icon: <SiX size={25}/>, link: 'https://twitter.com', color: '#000000', label: 'twitter'},
         {icon: <SiYoutube size={25}/>, link: 'https://youtube.com', color:'FF0000', label: 'Youtube'},
     ];
